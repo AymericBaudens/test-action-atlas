918aa690a03cfe734ce0839e6bad10ac54f1f249
Aymeric Baudens <aymeric.baudens@adeo.com>
2021-08-25 14:05:21 +0200

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+apitoken = ""
+base_url = "https://api.aiven.io/v1"
+aiven_project = "fras-t-tst"
+
+
+headers = {"Authorization": "aivenv1 " + apitoken}
+req_url = base_url + "/project/" + aiven_project + "/service"
+
+res = requests.get(
+    req_url,
+    headers=headers,
+)
+
+if res.status_code == 200:
+    json_data = json.loads(res.text)
+    for service in json_data["services"]:
+        for service_integration in service["service_integrations"]:
+            if service_integration["integration_type"] == "read_replica":
+                print(
+                    "Integration to be deleted: "
+                    + service_integration["service_integration_id"]
+                )
+                exit(0)
