6ec83ca0330a9ead0a0ed57775d6b7de67856936
Aymeric Baudens <aymeric.baudens@adeo.com>
2021-08-25 15:02:15 +0200

    .

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ aiven_project = "fras-t-tst"
 
 
 def main():
-    print("coucou")
+    print("coucou from python script")
     headers = {"Authorization": "aivenv1 " + apitoken}
     req_url = base_url + "/project/" + aiven_project + "/service"
 
