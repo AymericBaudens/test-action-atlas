f81a0b1016cb9a1c1c0fc18d08ab33b2788042a0
Aymeric Baudens <aymeric.baudens@adeo.com>
2021-08-25 14:19:40 +0200

    .

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import requests
 import json
 
-apitoken = ""
-base_url = "https://api.aiven.io/v1"
-aiven_project = "fras-t-tst"
+
+def main():
+    apitoken = ""
+    base_url = "https://api.aiven.io/v1"
+    aiven_project = "fras-t-tst"
+
+    headers = {"Authorization": "aivenv1 " + apitoken}
+    req_url = base_url + "/project/" + aiven_project + "/service"
+
+    res = requests.get(
+        req_url,
+        headers=headers,
+    )
+
+    if res.status_code == 200:
+        json_data = json.loads(res.text)
+        for service in json_data["services"]:
+            for service_integration in service["service_integrations"]:
+                if service_integration["integration_type"] == "read_replica":
+                    print(
+                        "Integration to be deleted: "
+                        + service_integration["service_integration_id"]
+                    )
+                    exit(0)
 
 
-headers = {"Authorization": "aivenv1 " + apitoken}
-req_url = base_url + "/project/" + aiven_project + "/service"
-
-res = requests.get(
-    req_url,
-    headers=headers,
-)
-
-if res.status_code == 200:
-    json_data = json.loads(res.text)
-    for service in json_data["services"]:
-        for service_integration in service["service_integrations"]:
-            if service_integration["integration_type"] == "read_replica":
-                print(
-                    "Integration to be deleted: "
-                    + service_integration["service_integration_id"]
-                )
-                exit(0)
+if __name__ == "__main__":
+    main()
