1ef62cabebc0bf126896d644cbf1288473076098
Aymeric Baudens <aymeric.baudens@adeo.com>
2021-08-25 15:49:24 +0200

    .

--- main.py
+++ main.py
@@ -6,23 +6,22 @@ base_url = "https://api.aiven.io/v1"
 aiven_project = "fras-t-tst"
 
 
-def main():
-    print("::set-output name=res::coucou")
-    headers = {"Authorization": "aivenv1 " + apitoken}
-    req_url = base_url + "/project/" + aiven_project + "/service"
+print("::set-output name=myres::coucou")
+headers = {"Authorization": "aivenv1 " + apitoken}
+req_url = base_url + "/project/" + aiven_project + "/service"
 
-    res = requests.get(
-        req_url,
-        headers=headers,
-    )
+res = requests.get(
+    req_url,
+    headers=headers,
+)
 
-    if res.status_code == 200:
-        json_data = json.loads(res.text)
-        for service in json_data["services"]:
-            for service_integration in service["service_integrations"]:
-                if service_integration["integration_type"] == "read_replica":
-                    print(
-                        "Integration to be deleted: "
-                        + service_integration["service_integration_id"]
-                    )
-                    exit(0)
+if res.status_code == 200:
+    json_data = json.loads(res.text)
+    for service in json_data["services"]:
+        for service_integration in service["service_integrations"]:
+            if service_integration["integration_type"] == "read_replica":
+                print(
+                    "Integration to be deleted: "
+                    + service_integration["service_integration_id"]
+                )
+                exit(0)
