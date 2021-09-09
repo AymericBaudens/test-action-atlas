1ae405f0ecb9fd816acacbec39c448c2a64eb80d
Aymeric Baudens <aymeric.baudens@adeo.com>
2021-09-09 17:20:49 +0200

    minor

--- main.py
+++ main.py
@@ -4,61 +4,59 @@ import os
 
 apitoken = os.environ["INPUT_TOKEN"]
 aiven_project = os.environ["INPUT_PROJECT"]
+src_service_name = os.environ["INPUT_SRC_SERVICE"]
+dst_service_name = os.environ["INPUT_DST_SERVICE"]
 # apitoken = ""
 # aiven_project = "fras-t-tst"
+# src_service_name = "primary-region-db"
+# dst_service_name = "recovery-region-db"
 base_url = "https://api.aiven.io/v1"
-euw1_service_name = "primary-region-db"
-euw4_service_name = "recovery-region-db"
 headers = {"Authorization": "aivenv1 " + apitoken}
 
-# print("::set-output name=myres::coucou " + aiven_project)
 
 req_url = base_url + "/project/" + aiven_project + "/service"
 
-reseuw1 = requests.get(
-    req_url + "/" + euw1_service_name,
+ressrc = requests.get(
+    req_url + "/" + src_service_name,
     headers=headers,
 )
-reseuw4 = requests.get(
-    req_url + "/" + euw4_service_name,
+resdst = requests.get(
+    req_url + "/" + dst_service_name,
     headers=headers,
 )
 
-if reseuw1.status_code == 200 and reseuw4.status_code == 200:
-    service_euw1 = json.loads(reseuw1.text)
-    service_euw4 = json.loads(reseuw4.text)
+if ressrc.status_code == 200 and resdst.status_code == 200:
+    service_src = json.loads(ressrc.text)
+    service_dst = json.loads(resdst.text)
 
-    service_euw1_integ_id = "euw1ko"
-    service_euw4_integ_id = "euw4ko"
+    service_src_integ_id = "srcko"
+    service_dst_integ_id = "dstko"
 
-    if "service_integrations" in service_euw1["service"]:
-        for service_integration in service_euw1["service"]["service_integrations"]:
+    if "service_integrations" in service_src["service"]:
+        for service_integration in service_src["service"]["service_integrations"]:
             if (
                 service_integration["integration_type"] == "read_replica"
                 and service_integration["active"]
+                and service_integration["dest_service"] == dst_service_name
             ):
-                service_euw1_integ_id = service_integration["service_integration_id"]
-    if "service_integrations" in service_euw4["service"]:
-        for service_integration in service_euw4["service"]["service_integrations"]:
+                service_src_integ_id = service_integration["service_integration_id"]
+    if "service_integrations" in service_dst["service"]:
+        for service_integration in service_dst["service"]["service_integrations"]:
             if (
                 service_integration["integration_type"] == "read_replica"
                 and service_integration["active"]
+                and service_integration["source_service"] == src_service_name
             ):
-                service_euw4_integ_id = service_integration["service_integration_id"]
-
-    # + erreur si services n existent pas
-    # + verifier sens de la replication
-    # + ko si plusieurs replications
-
-    if service_euw1_integ_id == service_euw4_integ_id:
-        print("Integration to be deleted: " + service_euw1_integ_id)
+                service_dst_integ_id = service_integration["service_integration_id"]
+    if service_src_integ_id == service_dst_integ_id:
+        print("Integration to be deleted: " + service_src_integ_id)
 
         req_url = (
             base_url
             + "/project/"
             + aiven_project
             + "/integration/"
-            + service_euw1_integ_id
+            + service_src_integ_id
         )
 
         res = requests.delete(
@@ -70,7 +68,8 @@ if reseuw1.status_code == 200 and reseuw4.status_code == 200:
         if res.status_code == 200:
             deletion = json.loads(res.text)
             if deletion["message"] == "deleted":
-                output = "youpi"
+                output = "dst DB promote master started. src DB will be shutted down"
+                # update service powered = false
     else:
         output = "not ready to switch"
 
